acbccfa46a714280e4612253ecd83401f0813fe6
Rajiv <rajivsraj78@gmail.com>
2023-02-18 18:45:14 +0530

    Product and Category List Displayed on Home page Working Fine

--- store/views.py
+++ store/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models.product import Product
+from .models.category import Category
 
 # Create your views here.
+def index(request):
+    products = Product.get_all_products()
+    categories = Category.get_all_categories()
+    data = {
+        "all_products": products,
+        "all_categories": categories
+    }
+    print(products)
+    return render(request, "index.html", data)
